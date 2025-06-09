025e98d9bc2cb83852fb8525ac733de89ca9844d
Thanassis Avgerinos <thanassis.avgerinos@gmail.com>
2025-06-09 04:48:38 -0400

    Attempt to work with compiler preprocessor.

--- helios_libfuzzer.c
+++ helios_libfuzzer.c
@@ -6,13 +6,19 @@
 #include <unistd.h>
 #include <limits.h>
 #include <ctype.h>
+#include <string.h>
+#include <assert.h>
+
+
+#define MAX_FUZZ_BUFFER_SIZE 1024000
 
 // ARG_MAX is the maximum length of a command invocation.
 // A single argument, being a string, has a minimum length
-// of 2 (except the last one), so we keep an array of half 
+// of 2 (except the last one), so we keep an array of half
 // the maximum length to account for all possible invocations.
 // In afl++ mode, 1024000 (afl buffer length) will replace ARG_MAX
 char *argv_max[1024000/2];
+char fuzz_buf[MAX_FUZZ_BUFFER_SIZE];
 
 int newargc;
 char ***senvp;
@@ -45,72 +51,106 @@ char ***senvp;
  -o argfuzz_libfuzzer.so ./argfuzz_libfuzzer.c \
  ~/path/to/libclang_rt.fuzzer_no_main-x86_64.a \
  -lstdc++ // Needs lib stdenv to work. Also see:\
- https://llvm.org/docs/LibFuzzer.html#using-libfuzzer-as-a-library  
+ https://llvm.org/docs/LibFuzzer.html#using-libfuzzer-as-a-library
 
 // Also compile PUT (two args in this case) as:\
- clang -g -fsanitize=fuzzer-no-link,address ./examples/libfuzzer/two_args_libfuzzer.c 
+ clang -g -fsanitize=fuzzer-no-link,address ./examples/libfuzzer/two_args_libfuzzer.c
 
-extern int LLVMFuzzerRunDriver(int *argc, char ***argv,\
-            int (*UserCb)(const uint8_t *Data, size_t Size));
+// extern int LLVMFuzzerRunDriver(int *argc, char ***argv,\
+//             int (*UserCb)(const uint8_t *Data, size_t Size));
 
-// extern int _helios__real_main(int, char **);
-int (*_helios__main_ptr)(int, char **, char **);
-int _helios__argparse(char *data, size_t data_len, int *argc, char **newargv);
+// // extern int _helios__real_main(int, char **);
+// int (*_helios__main_ptr)(int, char **, char **);
+// int _helios__argparse(char *data, size_t data_len, int *argc, char **newargv);
 
-int User_LLVMFuzzerTestOneInput(const uint8_t *Data, size_t Size) {
-    _helios__argparse((char *)Data, Size, &newargc, argv_max);
-    _helios__main_ptr(newargc, argv_max, *senvp);
-    return 0;
-}
+// int User_LLVMFuzzerTestOneInput(const uint8_t *Data, size_t Size) {
+//     _helios__argparse((char *)Data, Size, &newargc, argv_max);
+//     _helios__main_ptr(newargc, argv_max, *senvp);
+//     return 0;
+// }
 
+// extern void foo(void);
+
+// Isomorphism converting a single byte array into an array of null-terminated strings.
 int _helios__argparse(char *data, size_t data_len, int *argc, char **newargv){
-    unsigned int data_i, argv_i;
+    size_t data_i, argv_i;
     data_i = argv_i = 0;
+    // fprintf(stderr, "Parsing %zu bytes of data into arguments...\n", data_len);
+    // fflush(stderr);
+    assert(data_len + 1 < MAX_FUZZ_BUFFER_SIZE);
+    memcpy(fuzz_buf, data, data_len);
+    fuzz_buf[data_len] = '\0'; // Ensure the last byte is null-terminated
 
-    newargv[argv_i++] = (char *)data;
-    while (data_i < data_len - 1){
+    newargv[argv_i++] = (char *)fuzz_buf;
+    while (data_i + 1 < data_len){
         // Case for non \0 char
-        if (data[data_i++])
+        if (fuzz_buf[data_i++])
             continue;
-        newargv[argv_i++] = (char *)(&(data[data_i]));
+        newargv[argv_i++] = (char *)(&(fuzz_buf[data_i]));
     }
-    data[data_i] = '\0';
+    fuzz_buf[data_i] = '\0'; // Ensure the last argument is null-terminated
     newargv[argv_i] = NULL;
     *argc = argv_i;
     return 0;
 }
 
-// int (*arg_fuzz__main_ptr)(int, char **, char **);
+extern int helios__main(int, char **);
 
+int LLVMFuzzerTestOneInput(const uint8_t *Data, size_t Size) {
+    // Parse the input data into arguments
+    _helios__argparse((char *)Data, Size, &newargc, argv_max);
 
-int _helios__pre_main(int argc, char *argv[], char *envp[]){
-    senvp = &envp;
+    // print argc and all arguments in hex form
+    // fprintf(stderr, "Parsed %d arguments:\n", newargc);
+    // for (int i = 0; i < newargc; i++) {
+    //     fprintf(stderr, "argv[%d]: ", i);
+    //     for (char *p = argv_max[i]; *p; p++) {
+    //         fprintf(stderr, "%02x ", (unsigned char)*p);
+    //     }
+    //     fprintf(stderr, "\n");
+    // }
+    // fflush(stderr);
 
-    #ifndef ARG_MAX
-        try((ARG_MAX = sysconf(_SC_ARG_MAX)) == -1, "Failed to get ARG_MAX from system\n");
-    #endif
-
-    LLVMFuzzerRunDriver(&argc, &argv, User_LLVMFuzzerTestOneInput);
-
-        // arg_fuzz__argparse(buf, len, &mut_argc, mut_argv);
-        // arg_fuzz__main_ptr(mut_argc, mut_argv, envp);
-
-    return 0;
+    // Call the main function with the parsed arguments
+    // foo();
+    // fprintf(stderr, "Calling main with %d arguments main at %p...\n", newargc, helios__main);
+    // fprintf(stderr, "Contents of function pointer: %p\n", *helios__main);
+    // fflush(stderr);
+    int result = helios__main(newargc, argv_max);
+    // fprintf(stderr, "Result of main: %d\n", result);
+    // fflush(stderr);
+    return result;
 }
 
-// Function pointer to the original libc_start_main function
-int (*libc_start_main_orig)(int (*main)(int, char **, char **), int argc, char **ubp_av, void (*init)(void), void (*fini)(void), void (*rtld_fini)(void), void (*stack_end));
 
-int __libc_start_main(void * func_ptr, int argc, char * argv[], void (*init)(void), void (*fini)(void), void (*rtld_fini)(void), void (*stack_end)) {
- 
-    // Get the original libc_start_main function
-    libc_start_main_orig = dlsym(RTLD_NEXT, "__libc_start_main");
-    if (libc_start_main_orig == NULL) {
-        fprintf(stderr, "Error in `dlsym`: %s\n", dlerror());
-        exit(1);
-    }
+// int _helios__pre_main(int argc, char *argv[], char *envp[]){
+//     senvp = &envp;
 
-    // save main function and call our custom main override
-    _helios__main_ptr = func_ptr;
-    return libc_start_main_orig(_helios__pre_main, argc, argv, init, fini, rtld_fini, stack_end);
-}
+//     #ifndef ARG_MAX
+//         try((ARG_MAX = sysconf(_SC_ARG_MAX)) == -1, "Failed to get ARG_MAX from system\n");
+//     #endif
+
+//     LLVMFuzzerRunDriver(&argc, &argv, User_LLVMFuzzerTestOneInput);
+
+//         // arg_fuzz__argparse(buf, len, &mut_argc, mut_argv);
+//         // arg_fuzz__main_ptr(mut_argc, mut_argv, envp);
+
+//     return 0;
+// }
+
+// // Function pointer to the original libc_start_main function
+// int (*libc_start_main_orig)(int (*main)(int, char **, char **), int argc, char **ubp_av, void (*init)(void), void (*fini)(void), void (*rtld_fini)(void), void (*stack_end));
+
+// int __libc_start_main(void * func_ptr, int argc, char * argv[], void (*init)(void), void (*fini)(void), void (*rtld_fini)(void), void (*stack_end)) {
+
+//     // Get the original libc_start_main function
+//     libc_start_main_orig = dlsym(RTLD_NEXT, "__libc_start_main");
+//     if (libc_start_main_orig == NULL) {
+//         fprintf(stderr, "Error in `dlsym`: %s\n", dlerror());
+//         exit(1);
+//     }
+
+//     // save main function and call our custom main override
+//     _helios__main_ptr = func_ptr;
+//     return libc_start_main_orig(_helios__pre_main, argc, argv, init, fini, rtld_fini, stack_end);
+// }
